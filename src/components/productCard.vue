1bc897da2005be32ce7675d483e02162973102c6
Startemy <93431881+Startemy@users.noreply.github.com>
2022-02-15 19:13:35 +0300

    Начало Vue

---
+++ src/components/productCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="product-card__img">
+      <a href="#">
+        <img src="../assets/image/orders/for_man/blue_jaket.png" alt="product_1">
+      </a>
+      <form action="../views/cart.html">
+        <button type="submit">
+          <img src="../assets/image/head/cart_logo.svg" alt="cart_logo__btn">
+          Add to Cart
+        </button>
+      </form>
+    </div>
+    <article>
+      <h2><a href="#">ELLERY X M'O CAPSULE</a></h2>
+      <p>Known for her sculptural takes on traditional
+        tailoring,
+        Australian arbiter
+        of
+        cool
+        Kym Ellery teams up with Moda Operandi.</p>
+      <strong>$10000.00</strong>
+    </article>
+  </div>
+</template>
+
+<script>
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<main class="main">
+  <section class="billboard">
+    <div class="billboard__flex container">
+      <div class="billboard__title">
+          <h1>THE BRAND
+            <span>OF&nbsp;LUXERIOUS
+              <span class="billboard__title__fashion">FASHION</span>
+            </span>
+          </h1>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <nav class="stocks container">
+      <a href="#" class="stocks__fw">
+        <p class="stocks--width">30% off <span> for&nbsp;women</span></p>
+      </a>
+      <a href="#" class="stocks__fm">
+        <p class="stocks--width">hot deal <span>for&nbsp;men</span></p>
+      </a>
+      <a href="#" class="stocks__fk">
+        <p class="stocks--width">new arrivals <span>for&nbsp;kids</span></p>
+      </a>
+      <a href="#" class="stocks__ac">
+        <p class="stocks--width-ac">luxirous & trendy <span>accesories</span></p>
+      </a>
+    </nav>
+  </section>
+
+  <section class="product-card container">
+    <h2>Fetured Items</h2>
+    <p>Shop for items based on what we featured in this week</p>
+
+    <section class='products'>
+    <productCard />
+    </section>
+
+    <nav class="proposal__btn-catalog">
+      <a href="#">Browse All Product</a>
+    </nav>
+  </section>
+
+</main>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import productCard from '../components/productCard.vue'
 
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    productCard
   }
 }
+
 </script>
